6dd24769af5ea96223b7c0d936540beebd6c65ce
shafin1196 <shafin23105101196@diu.edu.bd>
2026-04-28 20:08:09 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/home_finder_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.home_finder_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/pages/my_post.dart
@@ -0,0 +1,89 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:go_router/go_router.dart';
+import 'package:home_finder_2/auth/api_service.dart';
+import 'package:home_finder_2/auth/loggedUser.dart';
+import 'package:home_finder_2/models/posts.dart';
+import 'package:home_finder_2/widgets/postItem.dart';
+
+class MyPosts extends StatefulWidget {
+  const MyPosts({super.key, required this.posts, required this.onDelete});
+  final List<Post> posts;
+  final Function onDelete;
+  @override
+  State<MyPosts> createState() => _MyPostsState();
+}
+
+class _MyPostsState extends State<MyPosts> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Posts"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        itemCount: widget.posts.length,
+                        itemBuilder: (context, index) {
+                          log(widget.posts[index].toString());
+                          final post = widget.posts[index];
+                          if(post.user!=LoggedUser.instance.user?.userId){
+                            return SizedBox.shrink();
+                          }
+                          log(index.toString());
+                          return Dismissible(
+                            key: Key(post.id.toString()),
+                            direction: DismissDirection.endToStart,
+                            background: Container(
+                              color: Color.fromARGB(255, 243, 247, 198),
+                              alignment: Alignment.centerRight,
+                              padding: EdgeInsets.symmetric(horizontal: 20),
+                              child: Icon(Icons.delete, color: Colors.red,size: 50,),
+                            ),
+                            onDismissed: (direction) async {
+                              EasyLoading.show(status: "Deleting Post...");
+                              final success = await ApiService.deletePost(post.id!);
+                              if (success) {
+                                widget.onDelete();
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('Post deleted successfully')),
+                                );
+                              } else {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(content: Text('Failed to delete post')),
+                                );
+                              }
+                              EasyLoading.dismiss();
+                            },
+                            child: PostItem(post: post));
+                        },
+                      ),
+            ),
+            ElevatedButton(onPressed: (){
+             context.push("/create-post");
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: const Color.fromARGB(255, 243, 247, 198),
+              foregroundColor: Colors.black,  
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0)
+              )
+             )
+            
+            , child: Text("Create New Post"))
+          ],
+        ),
+      )
+      );
+    
+  }
+}
